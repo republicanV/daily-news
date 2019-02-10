f734ef9ce31922856ec88f7b0dc9f8d1c4640f09
republicanV <v.sapovskiy@gmail.com>
2019-02-10 20:42:33 +0300

    finish basic components

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,16 +2,41 @@ import React from 'react';
 import './Stories.css';
 
 import Story from './Story';
+import StoriesHeader from './StoriesHeader';
+
+const COLUMNS = {
+    title: {
+        label: 'Title',
+        width: '40%'
+    },
+    author: {
+        label: 'Author',
+        width: '30%'
+    },
+    comments: {
+        label: 'Comments',
+        width: '10%'
+    },
+    points: {
+        label: 'Points',
+        width: '10%'
+    },
+    archives: {
+        label: 'Archives',
+        width: '10%'
+    }
+};
 
 const Stories = ({ stories }) => 
     <div className="stories">
+        <StoriesHeader columns={COLUMNS} />
         {(stories || []).map(story => 
             <Story
                 key={story.objectID}
                 story={story}
+                columns={COLUMNS}
             />
         )}
     </div>
 
 export default Stories;
- 
