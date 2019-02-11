dab97ddd13a7a6a11366c6cfdf1629da135353a9
v_sapovsky <v.sapovskiy@gmail.com>
2019-02-11 18:52:30 +0200

    add actions

--- src/index.js
+++ src/index.js
@@ -4,34 +4,22 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import { getReadableStories } from "./selectors/story";
-import { STORY_ARCHIVE } from "./constants/actionTypes";
+import { doArchiveStory } from "./actions/archive";
 import * as serviceWorker from './serviceWorker';
 
-// const stories = [
-//     {
-//       title: 'React',
-//       url: 'https://facebook.github.io/react/',
-//       author: 'Jordan Walke',
-//       num_comments: 3,
-//       points: 4,
-//       objectID: 0,
-//     }, {
-//       title: 'Redux',
-//       url: 'https://github.com/reactjs/redux',
-//       author: 'Dan Abramov, Andrew Clark',
-//       num_comments: 2,
-//       points: 5,
-//       objectID: 1,
-//     },
-//   ];
+function render() {
+    ReactDOM.render(
+        <App
+            stories={getReadableStories(store.getState())}
+            onArchive={id => store.dispatch(doArchiveStory(id))}
+        />,
+        document.getElementById('root')
+    );
+}
 
-ReactDOM.render(
-    <App
-        stories={getReadableStories(store.getState())}
-        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
-    />,
-    document.getElementById('root')
-);
+store.subscribe(render);
+render();
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
